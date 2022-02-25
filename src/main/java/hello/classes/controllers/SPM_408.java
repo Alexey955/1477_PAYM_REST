42b021b0899c1eb22f0c75a7a53ec13873ce63bb
Alexey_Vasilev <alekseyvasiljev@gmail.com>
2022-02-25 17:24:27 +0300

    V02 master

--- src/main/java/hello/classes/controllers/SPM_408.java
+++ src/main/java/hello/classes/controllers/SPM_408.java
@@ -32,9 +32,9 @@ public class SPM_408 {
 //        long timeStart = System.currentTimeMillis();
 
         //debug; наполнение объекта Request для запроса и записываем в файл
-        Request requestLog = new Request();
-        fillRequest(requestLog, request, headers, "false", body);
-        printLogs("./1477_PAYM_REST_logs/logs.txt", requestLog.toString());
+//        Request requestLog = new Request();
+//        fillRequest(requestLog, request, headers, "false", body);
+//        printLogs("./1477_PAYM_REST_logs/logs.txt", requestLog.toString());
 
         //Добавляем заголовки для ответа
         response.setHeader("Content-Type", "text/xml;charset=utf-8");
@@ -58,9 +58,9 @@ public class SPM_408 {
 //        responseLog.setBody(responseBody);
 
         //debug; наполнение объекта Request для ответа и записываем в файл
-        Request responseLog = new Request();
-        fillResponse(responseLog, requestLog, response, "false", responseBody);
-        printLogs("./1477_PAYM_REST_logs/logs.txt", responseLog.toString());
+//        Request responseLog = new Request();
+//        fillResponse(responseLog, requestLog, response, "false", responseBody);
+//        printLogs("./1477_PAYM_REST_logs/logs.txt", responseLog.toString());
 
         //Записываем в файл для дальнейшей отправки в InfluxDB
 //        StringToInfluxDB stringToInfluxDB = new StringToInfluxDB();
